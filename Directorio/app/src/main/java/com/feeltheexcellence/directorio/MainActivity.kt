17e8b58b9cf4cf3b9feeb257a054e4f6211bae7d
Priscila Medrano <PrissMedrano@users.noreply.github.com>
2021-04-12 00:14:12 -0500

    Leer datos desde la BD
    Cambio para copiar la BD y tomar los datos desde ahi

--- Directorio/app/src/main/java/com/feeltheexcellence/directorio/MainActivity.kt
+++ Directorio/app/src/main/java/com/feeltheexcellence/directorio/MainActivity.kt
@@ -1,17 +1,104 @@
 package com.feeltheexcellence.directorio
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.feeltheexcellence.directorio.database.DatabaseHandler
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
+    val MENU_HEALTH = "health"
+    val MENU_FOOD_BED = "food-bed"
+    val MENU_SUPPLIERS = "suppliers"
+
+    val URL_HEALTH = "html/menu/health.html"
+    val URL_FOOD_BED = "html/menu/food-bed.html"
+    val URL_SUPPLIERS = "html/submenu/suppliers.html"
+    val HOME = "file:///android_asset/index.html"
+    val MIME = "text/html"
+    val ENCONDING = "utf-8"
+    val BASE_URL = "file:///android_asset/"
+
+    var MENU = "menu"
+    var SUBMENU = "submenu"
+    var ELEMENTO = "elemento"
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val indexWebView : WebView
-        indexWebView = findViewById(R.id.indexWebView)
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+        val indexWebView: WebView = findViewById(R.id.indexWebView)
         indexWebView.settings.javaScriptEnabled = true;
         indexWebView.loadUrl("file:///android_asset/index.html")
+        val context = this;
+
+        indexWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                var result = false
+                var fileContent = ""
+                var url = request?.url.toString()
+
+                if(url.contains(SUBMENU)){
+                    result = true
+                    url = getUrl(url)
+                    fileContent = getFileContent(url)
+                    var listado = databaseHandler.getListado(url)
+                    fileContent = fileContent.replace("<LISTA_ELEMENTOS/>", listado)
+                }
+                else if(url.contains(MENU)){
+                    result = true
+                    url = getUrl(url)
+                    fileContent = getFileContent(url)
+                }
+                else if(url.contains(ELEMENTO)){
+                    result = true
+                    fileContent = getFileContent(getUrl(url))
+                    var listado = databaseHandler.getDatosElemento(url);
+                    fileContent = fileContent.replace("<DATOS_ELEMENTO/>", listado)
+                }
+
+                if(result){
+                    view?.loadDataWithBaseURL(BASE_URL, fileContent, MIME, ENCONDING, HOME)
+                }
+                return result
+            }
+        }
+    }
+    fun getUrl(originalURL: String): String{
+        var url = originalURL
+        url = url.replace("open://", "")
+        var secciones = url.split("/")
+        if(secciones[0] == MENU){
+            url = "html/menu/" + secciones[1]
+        }
+        else if(secciones[0] == SUBMENU){
+            url = "html/submenu/" + secciones[1]
+        }
+        else if(secciones[0] == ELEMENTO){
+            url = "html/elemento/" + secciones[1].toLowerCase() + ".html"
+        }
+        return url
+    }
+
+    fun getFileContent(url: String): String{
+        var fileContent = ""
+        try {
+            val inputStream: InputStream = assets.open(url)
+            val size: Int = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            fileContent = String(buffer)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        finally {
+            return fileContent
+        }
     }
 }
---
+++ Directorio/app/src/main/java/com/feeltheexcellence/directorio/database/DatabaseHandler.kt
@@ -0,0 +1,139 @@
+package com.feeltheexcellence.directorio.database
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+class DatabaseHandler(context: Context) : SQLiteOpenHelper(context, DB_NAME,null, DB_VERSION) {
+    val HEALTH = "html/menu/health.html"
+    val FOOD_BED = "html/menu/food-bed.html"
+    val SUPPLIERS = "html/submenu/suppliers.html"
+    var TABLA_HOSPITALES = "HOSPITALES"
+    var TABLA_MEDICOS = "MEDICOS"
+    val TABLA_SUPPLIERS = "MEDICALSUPPLIERS"
+    var context = context
+
+    companion object {
+        private var DB_VERSION = 1
+        private var DB_NAME = "FeelTheExcellenceDB.sqlite3"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        println("onCreate")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        println("onUpgrade")
+    }
+
+    fun getListado(tipo: String): String {
+        val db = openDatabase()
+        var sql = ""
+        var cursor: Cursor? = null
+        var listado = ""
+
+        if(tipo.equals(HEALTH)){
+
+        }
+        else if(tipo.equals(FOOD_BED)){
+
+        }
+        else if(tipo.equals(SUPPLIERS)){
+            sql = "SELECT id, empresa from MedicalSuppliers ORDER BY empresa asc"
+        }
+        try{
+            cursor = db.rawQuery(sql, null)
+        }catch (e: SQLiteException) {
+            db.execSQL(sql)
+        }
+        listado = "<div class=\"list-group\" id=\"list\">\n"
+        if (cursor?.moveToFirst()!!) {
+            do {
+                listado += "<a href=\"open://elemento/" + TABLA_SUPPLIERS + "/" + cursor.getInt(0) + "\" class=\"list-group-item list-group-item-action\">" + cursor.getString(1) + "</a>\n"
+            } while (cursor.moveToNext())
+        }
+        listado += "</div>"
+        return listado
+    }
+
+    fun getDatosElemento(url: String): String {
+        val db = openDatabase()
+        val sql = "SELECT * FROM " + getQueryElement(url, "tabla") + " WHERE ID = " + getQueryElement(url, "id")
+        var cursor: Cursor? = null
+        var datos = ""
+
+        try{
+            cursor = db.rawQuery(sql, null)
+        }catch (e: SQLiteException) {
+            db.execSQL(sql)
+        }
+        if (cursor?.moveToFirst()!!) {
+            do {
+                datos = " <div>\n" +
+                        "   <h4>" + cursor.getString(cursor.getColumnIndex("Empresa")) + "</h4>\n" +
+                        "   <p class=\"text-wrap\">"  + cursor.getString(cursor.getColumnIndex("Servicios")) + "</p>\n" +
+                        "   <p class=\"text-wrap\">" + cursor.getString(cursor.getColumnIndex("Direccion")) + "</p>\n" +
+                        "   <p class=\"text-wrap\"> Tel:" + cursor.getString(cursor.getColumnIndex("Telefono")) + "</p>\n" +
+                        "   <p class=\"text-wrap\">" + cursor.getString(cursor.getColumnIndex("Email")) + "</p>\n" +
+                        "   <p class=\"text-wrap\">" + cursor.getString(cursor.getColumnIndex("Website")) + "</p>\n" +
+                        " </div>\n" +
+                        " <div>\n" +
+                        "   <iframe src=" + cursor.getString(cursor.getColumnIndex("Ubicacion")) + "\n" +
+                        "       width=\"100%\" height=\"450\" frameborder=\"0\" style=\"border:0;\"\n" +
+                        "       allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>\n" +
+                        " </div>"
+            } while (cursor.moveToNext())
+        }
+        return datos
+    }
+
+    fun getQueryElement(originalURL: String, parte: String): String{
+        var url = originalURL
+        url = url.replace("open://", "")
+        var secciones = url.split("/")
+        var elemento = ""
+
+        if(parte == "tabla"){
+            elemento = secciones[1]
+        }
+        else if(parte == "id"){
+            elemento = secciones[2]
+        }
+        return elemento
+    }
+
+    fun openDatabase(): SQLiteDatabase {
+        val dbFile = context.getDatabasePath(DB_NAME)
+        try {
+            val checkDB = context.openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE,null)
+            val version = checkDB.version
+            checkDB?.close()
+            if (!dbFile.exists() || version < DB_VERSION) {
+                copyDatabase(dbFile)
+            }
+        } catch (e: IOException) {
+            throw RuntimeException("Error creating source database", e)
+        }
+        return SQLiteDatabase.openDatabase(dbFile.path, null, SQLiteDatabase.OPEN_READWRITE)
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun copyDatabase(dbFile: File) {
+        val istr = context.assets.open("databases/" + DB_NAME)
+        val ostr = FileOutputStream(dbFile)
+
+        val buffer = ByteArray(1024)
+        while (istr.read(buffer) > 0) {
+            ostr.write(buffer)
+        }
+        ostr.flush()
+        ostr.close()
+        istr.close()
+    }
+}
